e469155f5b7a8b3f3279b863abd2cef48f4c1643
Pushkar Aditya <pushkar.aditya@askiitians.com>
2016-06-14 18:01:59 +0530

    save point, pata nahi kaha par hooo

---
+++ Framework/app/widgets/dwaTemperature/dwaTemperatureDirective.js
@@ -0,0 +1,10 @@
+﻿"use strict";
+
+angular.module("app").directive("dwaTemperature", function (dataService) {
+  return {
+    templateUrl: 'app/widgets/dwaTemperature/dwaTemperatureTemplate.html',
+    link: function (scope, el, attrs) {
+
+    }
+  };
+});
